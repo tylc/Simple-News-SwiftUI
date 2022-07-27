67ba047d216836d6653fdf811a863d12b5977074
Enric Herce <enric.herce@cuatroochenta.com>
2022-07-27 13:02:31 +0200

    Initial Commit

---
+++ Simple News/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Simple News
+//
+//  Created by Enric Herce on 27/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple News/Simple_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_NewsApp.swift
+//  Simple News
+//
+//  Created by Enric Herce on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
